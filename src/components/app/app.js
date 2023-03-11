b6d44601a4d11ac1544f2fee7568e864875cbe5c
ArmAtrix123 <alexstor2003@gmail.com>
2023-03-11 18:29:49 +0300

    create: RiseUp

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,9 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 function App(){
     
 const data = [ 
-    {name: 'John C.', salary: 8000, increase: false, id: 1},
-    {name: 'Alex M', salary: 3000, increase: true, id: 2},
-    {name: 'Carl W.', salary: 5000, increase: false, id: 3},
+    {name: 'John C.', salary: 8000, increase: false, id: 1, like: false},
+    {name: 'Alex M', salary: 3000, increase: true, id: 2, like: true},
+    {name: 'Carl W.', salary: 5000, increase: false, id: 3, like: false},
     ];
 
     return (
